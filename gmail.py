be94e46d7513e130fb92e8b1987baaacbff7c45e
mushrifshahreyar <mushrifshahreyar>
2019-07-21 23:42:51 +0530

    initial

---
+++ gmail.py
@@ -0,0 +1,24 @@
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By 
+import time 
+
+driver = webdriver.Chrome()
+driver.get('https://mail.google.com/')
+wait = WebDriverWait(driver,5)
+arg = "//input[@class = 'whsOnd zHQkBf']"
+input_box = wait.until(EC.presence_of_element_located((By.XPATH,arg)))
+string = 'mushrifshahreyar'
+password = 'Tuttu@786'
+input_box.send_keys(string)
+input_box.send_keys(Keys.ENTER)
+time.sleep(3)
+input_box = wait.until(EC.presence_of_element_located((By.XPATH,arg)))
+input_box.send_keys(password)
+input_box.send_keys(Keys.ENTER)
+time.sleep(3)
+arg = "//div[@class='T-I J-J5-Ji T-I-KE L3']"
+input_box = wait.until(EC.presence_of_element_located((By.XPATH,arg)))
+input_box.click()
---
+++ whatsapp.py
@@ -0,0 +1,31 @@
+from selenium import webdriver 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By 
+import time 
+  
+# Replace below path with the absolute path 
+# to chromedriver in your computer 
+driver = webdriver.Chrome() 
+  
+driver.get("https://web.whatsapp.com/") 
+wait = WebDriverWait(driver, 20) 
+  
+# Replace 'Friend's Name' with the name of your friend  
+# or the name of a group  
+target = ''
+  
+# Replace the below string with your own message 
+string = "Happy Bday Mamujan...."
+  
+x_arg = "//span[@title ='Uncle']"
+group_title = wait.until(EC.presence_of_element_located(( 
+    By.XPATH, x_arg))) 
+group_title.click() 
+inp_xpath = "//div[@class ='_2S1VP copyable-text selectable-text']"
+input_box = wait.until(EC.presence_of_element_located(( 
+    By.XPATH, inp_xpath))) 
+for i in range(50): 
+    input_box.send_keys(string + Keys.ENTER) 
+    time.sleep(1) 
